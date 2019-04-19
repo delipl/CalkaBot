79a60f54e19779889dc8e017918bb7bde8a5f111
delipl <delicat.kuba@gmail.com>
2019-04-19 02:48:11 +0200

    dodanie header.h wszystkie #define funkcje main.cpp

---
+++ src/header.h
@@ -0,0 +1,155 @@
+#include <Arduino.h>
+
+uint8_t full = 255;
+uint8_t half = 255/2;
+bool direct  = true;
+
+  //Leds on board
+	#define builtLed1 		(1<<PE2)
+	#define builtLed2		  (1<<PE6)
+	#define builtLed1_ON	PORTE |= builtLed1  	// |0000| | 0001 = |0001|
+	#define builtLed2_ON	PORTE |= builtLed2
+	#define builtLed1_OFF	PORTE &= ~builtLed1 	// |0001| & ~0001 = 0001 & 1110 = |0000|
+	#define builtLed2_OFF	PORTE &= ~builtLed2
+	#define builtLed1_TOG	PORTE ^= builtLed1 		// |0000| ^ 0001 = |0001|
+	#define builtLed2_TOG	PORTE ^= builtLed2 		// |1000| ^ 1000 = |0000|
+
+  //floor sensors
+  #define FaR 			(1<<PD0)
+  #define FaL				(1<<PD1)
+  #define FbR				(1<<PD3)
+  #define FbL				(1<<PD2)
+
+  #define edgeA     ((PIND & FaL) | (PIND & FaR))
+  #define edgeB     ((PIND & FbL) | (PIND & FbR))
+  #define edgeAL    (PIND & FaL)
+  #define edgeAR    (PIND & FaR)
+  #define edgeBL    (PIND & FbL)
+  #define edgeBR    (PIND & FbR)
+
+  bool foor(){
+    return  !edgeA  ||
+            !edgeAL ||
+            !edgeAR ||
+            !edgeB  ||
+            !edgeBR ||
+            !edgeBL ||;
+  }
+
+  //buttons
+  #define but1      (1<<PF0)
+  #define but2      (1<<PF1)
+  #define but3      (1<<PF4)
+
+/*
+  #define butConf1    ((PINF & but1) | (PINF & but2) | (PINF & but3))
+  #define butConf2    (((PINF & but1) | (PINF & but2)) & (PINF & ~but3))
+  #define butConf3    ((PINF & but1) & (PINF & ~but2) & (PINF & ~but3))
+  #define butConf4    ((PINF & but1) & (PINF & but2) & (PINF & but3))
+  #define butConf5    ((PINF & ~but1) & ((PINF & but2) | (PINF & but3)))
+  #define butConf6    ((PINF & ~but1) | (PINF & ~but2) | (PINF & but3))
+  #define butConf7    ((PINF & ~but1) | (PINF & but2) | (PINF & ~but3))
+  #define butConf8    ((PINF & but1) | (PINF & ~but2) | (PINF & but3))
+*/
+
+  //distans Sensors
+  #define IRa       (1<<PF6)
+  #define IRaR      (1<<PF5)
+  #define IRaL      (1<<PF7)
+  #define IRb       (1<<PD4)
+  #define IRbR      (1<<PD5)
+  #define IRbL      (1<<PD6)
+
+  #define disA      (PINF & IRa)
+  #define disAR     (PINF & IRaR)
+  #define disAL     (PINF & IRaL)
+  #define disB      (PIND & IRb)
+  #define disBR     (PIND & IRbR)
+  #define disBL     (PIND & IRbL)
+
+  //control motor A PB
+	#define Ma1				(1<<PB5)
+	#define Ma2				(1<<PB4)
+	#define MaPWM			(1<<PD7)
+
+	#define Ma1_ON		PORTB |= Ma1
+	#define Ma2_ON	 	PORTB |= Ma2
+	#define Ma1_OFF		PORTB &= ~Ma1
+	#define Ma2_OFF	 	PORTB &= ~Ma2
+	#define	MaPWM_ON	PORTD |= MaPWM
+
+  //control motor B
+  #define Mb1				(1<<PB6)
+  #define Mb2				(1<<PC6)
+  #define MbPWM			(1<<PC7)
+
+	#define Mb1_ON		  PORTB |= Mb1
+	#define Mb2_ON	 		PORTC |= Mb2
+	#define Mb1_OFF		  PORTB &= ~Mb1
+	#define Mb2_OFF	 		PORTC &= ~Mb2
+	#define	MbPWM_ON		PORTC |= MbPWM
+
+  //control motors
+  void go(uit8_t x, uint8_t y){
+    analogWrite(x, MaPWM);
+    analogWrite(y, MbPWM);
+  	Ma1_OFF;
+  	Ma2_ON;
+  	Mb1_OFF;
+  	Mb2_ON;
+  }
+
+  void goBack(uit8_t x, uint8_t y){
+    analogWrite(x, MaPWM);
+    analogWrite(y, MbPWM);
+    Ma1_ON;
+  	Ma2_OFF;
+  	Mb1_ON;
+  	Mb2_OFF;
+  }
+
+  void hardStop(){
+  	Ma1_ON;
+  	Ma2_ON;
+  	Mb1_ON;
+  	Mb2_ON;
+  }
+
+  void slowStop(){
+  	Ma1_OFF;
+  	Ma2_OFF;
+  	Mb1_OFF;
+    Mb2_OFF;
+  }
+
+  //pins ins and outs
+  void pins(){
+    //output ports for motor A control
+  	DDRD |= MaPWM;
+    DDRB != Ma1 | Ma2;
+
+    //output ports for motor B control
+  	DDRC |= MbPWM | Mb2;
+    DDRB |= Mb1;
+
+    //output ports for leds
+  	DDRE |= builtLed1 | builtLed2;
+
+  	//input ports floor sensor
+    DDRD 	&= ~FaR & ~FaL & ~FbR & ~FaL;
+    //disconnect inside VCC
+    PORTD &= ~FaR & ~FaL & ~FbR & ~FaL;
+
+    //input ports for buttons
+    DDRF  &= ~but1 & ~but2 & ~but3;
+    //disconnect insice VCC
+    PORTF &= ~but1 & ~but2 & ~but3;
+
+    //input portys for distans sensors
+    DDRF &= ~IRa & ~IRaR & ~IRaL;
+    DDRD &= ~IRb & ~IRbR & ~IRbL;
+
+    //disconect inside VCC
+    PORTF &= ~IRa & ~IRaR & ~IRaL;
+    PORTD &= ~IRb & ~IRbR & ~IRbL;
+  }
--- src/main.cpp
+++ src/main.cpp
@@ -1,149 +1,45 @@
-#include <Arduino.h>
+#include "header.h"
 
-
-//control motor A PB
-	#define Ma1				(1<<PB5)
-	#define Ma2				(1<<PB4)
-	#define MaPWM			(1<<PD7)
-
-	#define Ma1_ON		  PORTB |= Ma1
-	#define Ma2_ON	 		PORTB |= Ma2
-	#define Ma1_OFF		  PORTB &= ~Ma1
-	#define Ma2_OFF	 		PORTB &= ~Ma2
-	#define	MaPWM_ON		PORTD |= MaPWM
-
-
-
-//control motor B
-  #define Mb1				(1<<PB6)
-  #define Mb2				(1<<PC6)
-  #define MbPWM			(1<<PC7)
-
-	#define Mb1_ON		  PORTB |= Mb1
-	#define Mb2_ON	 		PORTC |= Mb2
-	#define Mb1_OFF		  PORTB &= ~Mb1
-	#define Mb2_OFF	 		PORTC &= ~Mb2
-	#define	MbPWM_ON		PORTC |= MbPWM
-
- //control motors
-	void goFoward(){
-  		Ma1_OFF;
-  		Ma2_ON;
-  		Mb1_OFF;
-  		Mb2_ON;
-  	}
-  	void goBackward(){
-  		Ma1_ON;
-  		Ma2_OFF;
-  		Mb1_ON;
-  		Mb2_OFF;
-  	}
-  	void hardStop(){
-  		Ma1_ON;
-  		Ma2_ON;
-  		Mb1_ON;
-  		Mb2_ON;
-  	}
-  	void slowStop(){
-  		Ma1_OFF;
-  		Ma2_OFF;
-  		Mb1_OFF;
-  		Mb2_OFF;
-  	}
-
-
-
-
-//Leds on board
-	#define builtLed1 		(1<<PE2)
-	#define builtLed2		  (1<<PE6)
-	#define builtLed1_ON	PORTE |= builtLed1  	// |0000| | 0001 = |0001|
-	#define builtLed2_ON	PORTE |= builtLed2
-	#define builtLed1_OFF	PORTE &= ~builtLed1 	// |0001| & ~0001 = 0001 & 1110 = |0000|
-	#define builtLed2_OFF	PORTE &= ~builtLed2
-	#define builtLed1_TOG	PORTE ^= builtLed1 		// |0000| ^ 0001 = |0001|
-	#define builtLed2_TOG	PORTE ^= builtLed2 		// |1000| ^ 1000 = |0000|
-
-//floor sensors
-  #define FaR 			  (1<<PD0)
-  #define FaL				  (1<<PD1)
-  #define FbR				  (1<<PD3)
-  #define FbL				  (1<<PD2)
-
-  #define edgeA   ((PIND & FaL) | (PIND & FaR))
-  #define edgeB   ((PIND & FbL) | (PIND & FbR))
-
-//buttons
-  #define but1        (1<<PF0)
-  #define but2        (1<<PF1)
-  #define but3        (1<<PF4)
-/*
-  #define butConf1    ((PINF & but1) | (PINF & but2) | (PINF & but3))
-  #define butConf2    (((PINF & but1) | (PINF & but2)) & (PINF & ~but3))
-  #define butConf3    ((PINF & but1) & (PINF & ~but2) & (PINF & ~but3))
-  #define butConf4    ((PINF & but1) & (PINF & but2) & (PINF & but3))
-  #define butConf5    ((PINF & ~but1) & ((PINF & but2) | (PINF & but3)))
-  #define butConf6    ((PINF & ~but1) | (PINF & ~but2) | (PINF & but3))
-  #define butConf7    ((PINF & ~but1) | (PINF & but2) | (PINF & ~but3))
-  #define butConf8    ((PINF & but1) | (PINF & ~but2) | (PINF & but3))
-*/
 void setup(){
-
-	DDRB = 0xF0;
-	DDRD |= MaPWM;
-	DDRC |= MbPWM;
-	DDRE |= builtLed1 | builtLed2;
-
-
-	//turning on board
-  builtLed2_ON;
+	//turning on boarD
 	for (int i = 0; i < 5; ++i){
 		delay(100);
 		builtLed1_TOG;
     builtLed2_TOG;
 	}
-  builtLed1_OFF;
-  builtLed2_OFF;
-  //===========================
 
-  //input ports floor sensor
-  DDRD 	&= ~FaR & ~FaL & ~FbR & ~FaL;
-  //disconnect inside VCC
-  PORTD &= ~FaR & ~FaL & ~FbR & ~FaL;
+//pin mode
+pins();
 
-  //input ports for buttons
-  DDRF  &= ~but1 & ~but2 & ~but3;
-  //disconnect insice VCC
-  PORTF &= ~but1 & ~but2 & ~but3;
-
-
-
-  	MaPWM_ON;
-  	MbPWM_ON;
+//start
+go(full, full);
 }
+
 void loop(){
+	//edge checker
+	if(!edgeA)							goBack(full, full);
+	else if(!edgeAL) 				goBack(full, half);
+	else if(!edgeAR)				goBack(half, full);
+	else if(!edgeB)					go(full, full);
+	else if(!edgeBL) 				go(full, half);
+	else if(!edgeBR)				go(half, full);
 
-}
+	//straight attack
+	else if(!IRa)						go(full, full);
+	else if(!IRb)						goBack(full, full);
 
-
-
-
-
-/*void two(int x){
-
-  bool bin[8];
-	for(int i = 7; i >= 0; i--){
-		if(x-pow(2,i) > 0){
-			bin[i] = 1;
-			x -= pow(2,i);
-		}
-			else{
-				bin[i] = 0;
-			}
+	//take a enemy
+	else if(!IRaR){
+		while(IRa || floor())	go(full, 0);
 	}
-	for(int i = 5; i >= 0; i--){
-		Serial.print(bin[i]);
+	else if(!IRaL){
+		while(IRa || floor())	go(0, full);
 	}
-	Serial.println();
+	else if(!IRbR){
+		while(IRb || floor())	goBack(full, 0);
+	}
+	else if(!IRbL){
+		while(IRb || floor())	goBack(0, full);
+	}
+	else 										go(full, full);
 }
-*/
