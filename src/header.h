cdc7dd30a151174811ebf55a9436004bee691cd9
JakubKivi <panmcleon@gmail.com>
2019-04-20 22:07:49 +0200

    Sprawdzanie switchy i podzial na podprogramy

--- src/header.h
+++ src/header.h
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 
-uint8_t full = 175;
+uint8_t full = 255;
+uint8_t optimal =200; //max speed, not to fligh away from ring
 uint8_t half = 180;
 bool direct  = true;
+uint8_t program = 0;
 
   //Leds on board
 	#define builtLed1 		(1<<PE2)
---
+++ src/program0.h
@@ -0,0 +1,5 @@
+program0(){
+  while(true){
+
+  }
+}
