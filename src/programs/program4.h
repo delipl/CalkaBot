309160fddc43e9ba1420ec5609e95f2c5177373b
delipl <delicat.kuba@gmail.com>
2019-04-25 04:47:19 +0200

    nie testowany linefolowwer

--- src/programs/program4.h
+++ src/programs/program4.h
@@ -1,18 +1,12 @@
 void program4(){
   while(true){
-      if(floorSensors(direction)){
-        go(3);
+      if(!edgeAL && !edgeBL){
+        while(!edgeAL && !edgeBL){
+          go(3);
+        }
       }else{
-          if(!disA){
-          direction=1;
-          go(0);
-          }
-          else if(!disB){
-            direction=0;
-            go(0);
-          }
-          else if(!disAR || !disAL || !disBL || !disBR) if(toSeeFront()) go(0);
-          else go(1);
+        go(0);
       }
-  }
+
+    }
 }
