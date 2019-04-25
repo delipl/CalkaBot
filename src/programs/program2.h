c1a3142dedebef2bfe9dad33c9b674641e0a0b31
JakubKivi <panmcleon@gmail.com>
2019-04-25 04:42:04 +0200

    spinner taki co powinien dzialac

--- src/programs/program2.h
+++ src/programs/program2.h
@@ -6,30 +6,26 @@ void program2(){
 
     while(true){
 
-      if(floorSensors(direction)){
-
-          hardStop();
-          delay(2);
-          direction_TOG;
-          go(0);
-          delay(50);
-      }
-      else if(!disA){
-        direction=1;
-        go(0);
-      }
-      else if(!disB){
-        direction=0;
-        go(0);
-      }
-      /*else if(!disAR || !disAL || !disBL || !disBR){
-        if(toSeeFront())
-          go(0);
-        }*/
-      else {
-       spin();
-      //hardStop();
-      //go(1);
-      }
+      if(!floorSensors(direction)){
+        if(!disA||!disB){
+            if(!disA)direction=1;
+            else if(!disB) direction=0;
+            go(0);
+        }else if(!disAR || !disAL || !disBL || !disBR){
+            if(!toSeeFront()){
+              hardStop();
+              direction_TOG;
+              go(0);
+              delay(100);
+            }
+        }else{
+          spin();
+        }
+    }else{
+      hardStop();
+      direction_TOG;
+      go(0);
+      delay(400);
     }
+}
 }
