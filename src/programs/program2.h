96c2233c0f95ad121558a6c02bc2833ab1c00ffd
delipl <delicat.kuba@gmail.com>
2019-05-15 15:28:59 +0200

    dzialajacy seeFront na tyl w program2()

--- src/programs/program2.h
+++ src/programs/program2.h
@@ -1,36 +1,21 @@
+/*
+jest tutaj dzialajacy seefront tylko ze z tylu
+*/
 void program2(){
-    int toBeMiddle=300;
-    go(0);
-    delay(toBeMiddle);
-    hardStop();
-
-    while(true){
-
-      if(!floorSensors(direction)){
-        if(!disA||!disB){
-            while(!disA){
-              direction=1;
-              go(0);
-            }
-            while(!disB){
-              direction=0;
-              go(0);
-            }
-        }else if(!disAR || !disAL || !disBL || !disBR){
-            if(!toSeeFront()){
-              hardStop();
-              direction_TOG;
-              go(0);
-              delay(100);
-            }
-        }else{
-          spin();
-        }
-    }else{
-      hardStop();
-      direction_TOG;
-      go(0);
-      delay(400);
-    }
-}
+  if(!disB){
+     goBackward();
+  }
+  else  if(!disBR){
+    Ma1_ON;
+    Ma2_OFF;
+    Mb1_OFF;
+    Mb2_OFF;
+  }
+  else if(!disBL){
+    Ma1_OFF;
+    Ma2_OFF;
+    Mb1_ON;
+    Mb2_OFF;
+  }
+  else slowStop();
 }
