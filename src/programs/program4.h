95e9edc27da6a2402eda1f06b7c483b84ca61f78
delipl <delicat.kuba@gmail.com>
2019-05-18 00:41:07 +0200

    dodano waitera

--- src/programs/program4.h
+++ src/programs/program4.h
@@ -1,12 +1,57 @@
 void program4(){
-  while(true){
-      if(!edgeAL && !edgeBL){
-        while(!edgeAL && !edgeBL){
-          slowSpin();
-        }
-      }else{
-        go(0);
-      }
-
+  if(first){
+    while(!floorSensors()) {
+      direction == true;
+      go(1);
     }
+    hardStop();
+    delay(200);
+  }
+
+  if(!disB && disA){
+    while((!disB && disA) && readButton()){
+      goBackward();
+    }
+  }
+  else if(!disA && disB){
+    while((!disA && disB) && readButton()){
+      goForward();
+    }
+  }
+  if(!disBR){
+    while(disB && readButton()){
+      Ma1_ON;
+      Ma2_OFF;
+      Mb1_OFF;
+      Mb2_OFF;
+    }
+  }
+  else if(!disBL){
+    while(disB && readButton()){
+      Ma1_OFF;
+      Ma2_OFF;
+      Mb1_ON;
+      Mb2_OFF;
+    }
+  }
+  else if(!disAL){
+    while(disA && readButton()){
+      Ma1_OFF;
+      Ma2_OFF;
+      Mb1_OFF;
+      Mb2_ON;
+    }
+  }
+  else if(!disAR){
+    while(disA && readButton()){
+      Ma1_OFF;
+      Ma2_ON;
+      Mb1_OFF;
+      Mb2_OFF;
+    }
+  }
+  else{
+
+    slowStop();
+  }
 }
