9a2dd38da6935cc3d7eace87395bd9a1129dff68
AWieczarek <adamwie12@gmail.com>
2020-10-10 20:08:42 +0200

    Basic display objects

--- simulation/main.py
+++ simulation/main.py
@@ -1,8 +1,23 @@
 import pygame, sys
+from pygame.locals import *
+
+HEIGHT = 1000
+WIDTH = 1000
+
+def draw_bg():
+    bg_rect.center = (HEIGHT/2, WIDTH/2)
+    screen.blit(bg_surface, bg_rect)
 
 pygame.init()
-screen  = pygame.display.set_mode((1000,1000))
+screen  = pygame.display.set_mode((HEIGHT, WIDTH))
 clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load("assets/board.png").convert_alpha()
+bg_rect = bg_surface.get_rect()
+
+robot_surface = pygame.image.load("assets/robot.png").convert_alpha()
+robot_rect = robot_surface.get_rect()
+
 
 while True:
     for event in pygame.event.get():
@@ -10,5 +25,10 @@ while True:
             pygame.quit()
             sys.exit()
 
+    draw_bg()
+
+    robot_rect.center = (HEIGHT/2, WIDTH/2)
+    screen.blit(robot_surface, robot_rect)
+
     pygame.display.update()
-    clock.tick(120)
+    clock.tick(60)
