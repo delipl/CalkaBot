1db34250e1d8eefcfbf7e0bba5d8c4be94ca0da5
delipl <delicat.kuba@gmail.com>
2020-10-12 21:10:47 +0200

    Acceleromoeter works fine

---
+++ calkabot3d0/src/accelerometer.h
@@ -0,0 +1,68 @@
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+
+void checkSettings()
+{
+  Serial.println();
+  
+  Serial.print(" * Sleep Mode:            ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+  
+  Serial.print(" * Clock Source:          ");
+  switch(mpu.getClockSource())
+  {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+  
+  Serial.print(" * Accelerometer:         ");
+  switch(mpu.getRange())
+  {
+    case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
+    case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
+    case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
+    case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
+  }  
+
+  Serial.print(" * Accelerometer offsets: ");
+  Serial.print(mpu.getAccelOffsetX());
+  Serial.print(" / ");
+  Serial.print(mpu.getAccelOffsetY());
+  Serial.print(" / ");
+  Serial.println(mpu.getAccelOffsetZ());
+  
+  Serial.println();
+}
+void accInit(){
+    while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+    {
+        Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+        delay(500);
+    }
+    //mpu.setAccelOffsetX();
+    //mpu.setAccelOffsetY();
+    //mpu.setAccelOffsetZ();
+  
+    checkSettings();
+}
+
+Vector getAcc(){
+    return mpu.readNormalizeAccel();
+} 
+
+void printAcc(){
+    Vector normAccel = getAcc();
+    Serial.print(" Xnorm = ");
+    Serial.print(normAccel.XAxis);
+    Serial.print(" Ynorm = ");
+    Serial.print(normAccel.YAxis);
+    Serial.print(" Znorm = ");
+    Serial.println(normAccel.ZAxis);
+}
--- calkabot3d0/src/main.cpp
+++ calkabot3d0/src/main.cpp
@@ -1,7 +1,12 @@
 #include "header.h"
 void setup(){
-
+  Serial.begin(9600);
+  while(!Serial.available()){
+    Serial.println();
+  }
+  Serial.println("Połączono...");
+  delay(500);
 }
-void main(){
+void loop(){
 
 }
