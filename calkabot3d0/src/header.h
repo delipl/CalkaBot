73cdb835f6bd7379518a6c2ff9deae41afcf3f25
delipl <delicat.kuba@gmail.com>
2020-10-12 23:22:17 +0200

    Accelerometer shows angle thanks to example for MPU6050 from https://github.com/jrowberg/i2cdevlib

--- calkabot3d0/src/header.h
+++ calkabot3d0/src/header.h
@@ -1,3 +1,5 @@
 #include <Arduino.h>
 #include "distance.h"
+
+
 #include "accelerometer.h"
--- calkabot3d0/src/main.cpp
+++ calkabot3d0/src/main.cpp
@@ -1,12 +1,20 @@
 #include "header.h"
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200);
   while(!Serial.available()){
     Serial.println();
   }
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+      Wire.begin();
+      Wire.setClock(400000); 
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+  #endif
+
   Serial.println("Połączono...");
-  delay(500);
+  initGyro();
 }
 void loop(){
-
+  printGyro();
+  delay(100);
 }
